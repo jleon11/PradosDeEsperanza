c4b82e35db90264223f9512cc622ace677ea6de6
justin.leon <justin.leon@inec.go.cr>
2025-05-13 18:59:46 -0600

    Ultima Contruccion para la web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,14 +34,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "49c728654158f7a6264d6c7aefbb96e2
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "64ae446da831e7b8504892fcc7d0eeab",
+"flutter_bootstrap.js": "56777ecf74f8d550699a044925f000cf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "4c0f7f59abbb266c4ef0cd9652fa2576",
 "/": "4c0f7f59abbb266c4ef0cd9652fa2576",
-"main.dart.js": "e4b84b1e72fd38554de8fe79f30c9bf3",
+"main.dart.js": "f5cf9c1adb7c811a6bfa219c5e0c724c",
 "manifest.json": "18b05de58f5aeed2ffa6be1246cc43f1",
 "version.json": "53fb59e54fb87bb44dba7c2200e1dead"};
 // The application shell files that are downloaded before a service worker can
